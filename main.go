7cc60bb7743ddb317bdad5a34aa005788dde1c5f
Dawid Duraj <dawid.duraj@outlook.de>
2025-05-26 22:05:21 +0200

    Add initial YouTube API setup

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "os"
+    "github.com/gin-gonic/gin"
+    "google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+    
 
 func main() {
     r := gin.Default()
     r.GET("/", func(c *gin.Context) {
-        c.String(200, "Hello, Gin!")
+        handle := c.Query("handle")
+        apiKey := os.Getenv("YOUTUBE_API_KEY")
+
+        if apiKey == "" {
+            c.JSON(500, gin.H{"error": "API key is not set"})
+            return
+        }
+
+        if handle == "" {
+            c.JSON(400, gin.H{"error": "Handle is required"})
+            return
+        }
+
+
+        _, _ = youtube.NewService(c, option.WithAPIKey(apiKey));
+
+        c.String(200, "Hello, Gin! Handle: %s", handle)
     })
     r.Run(":8080") // Starts the server
 }
